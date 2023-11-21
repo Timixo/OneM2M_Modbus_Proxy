082e47b268e0e447cc51af85efa27d49b1fab9ca
Tim De Jonghe <51677515+Timixo@users.noreply.github.com>
2023-11-22 02:43:57 +0900

    Started work with API (WIP)

---
+++ Modbus_Interworking_Proxy/Controllers/ModbusDeviceController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Modbus_Interworking_Proxy.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ModbusDeviceController : ControllerBase
+    {
+        [HttpGet(Name = "GetTest")]
+        public ObjectResult GetTest()
+        {
+            return Ok("Test");
+        }
+    }
+}
