309210538988c9b14242d007b048c0f5010ce440
Condorbox <minecraft13122001@gmail.com>
2023-12-07 00:22:01 +0900

    test SlaveNetwork

--- Modbus_Interworking_Proxy/Services/ModbusService.cs
+++ Modbus_Interworking_Proxy/Services/ModbusService.cs
@@ -1,6 +1,8 @@
 ﻿using Modbus_Interworking_Proxy.Models;
 using NModbus;
+using NModbus.Device;
 using NModbus.Message;
+using System.Net;
 using System.Net.Sockets;
 
 namespace Modbus_Interworking_Proxy.Services
@@ -10,6 +12,7 @@ namespace Modbus_Interworking_Proxy.Services
         private readonly TcpClient _client;
         private readonly ModbusFactory _factory;
         private readonly IModbusMaster _modbusMaster;
+        private readonly IModbusSlaveNetwork _modbusSlaveNetwork;
 
         public ModbusService(string ip, int port)
         {
@@ -18,15 +21,20 @@ namespace Modbus_Interworking_Proxy.Services
                 _client = new TcpClient(ip, port);
                 _factory = new ModbusFactory();
                 _modbusMaster = _factory.CreateMaster(_client);
+                IPAddress ipAddress = IPAddress.Parse(ip);
+                // TODO Maybe we should create this outside and when we don't want to use it anymore close it
+                TcpListener tcpListener = new TcpListener(ipAddress, port);
+                _modbusSlaveNetwork = _factory.CreateSlaveNetwork(tcpListener);
             }
             catch(Exception)
             {
                 return;
-            }
+            } 
         }
 
         public void Dispose()
         {
+            _modbusSlaveNetwork.Dispose();
             _modbusMaster.Dispose();
             _client.Dispose();
         }
@@ -37,6 +45,9 @@ namespace Modbus_Interworking_Proxy.Services
             {
                 // model.Id doesn't do anything yet, we need to figure out how to get data from a device with the slave id, which is not the same as slave address
                 ushort[] data = _modbusMaster.ReadHoldingRegisters(model.Id, 0, 1);
+
+                IModbusSlave slave = _factory.CreateSlave(model.Id);
+                _modbusSlaveNetwork.AddSlave(slave);
                 return model;
             }
             catch(Exception)
@@ -49,6 +60,7 @@ namespace Modbus_Interworking_Proxy.Services
         {
             try
             {
+                _modbusSlaveNetwork.GetSlave(slaveId);
                 return _modbusMaster.ReadHoldingRegisters(slaveId, startAddress, length);
             }
             catch (Exception ex)
